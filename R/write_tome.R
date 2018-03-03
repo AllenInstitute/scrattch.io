baca59a57ace861798c605fed5b09f6585a1de96
Lucas Gray <soulcatalyst@gmail.com>
2018-03-02 16:42:00 -0800

    Shifted sparse data matrices to the /data/ group.

--- R/write_tome.R
+++ R/write_tome.R
@@ -9,11 +9,11 @@
 #' @param compression_level The data compression level for large HDF5 matrix objects. default = 4.
 #'
 write_tome_data <- function(exon_mat = NULL,
-                             intron_mat = NULL,
-                             tome = "counts.tome",
-                             cols_are = "sample_name",
-                             overwrite = F,
-                             compression_level = 4) {
+                            intron_mat = NULL,
+                            tome = "counts.tome",
+                            cols_are = "sample_name",
+                            overwrite = F,
+                            compression_level = 4) {
 
   library(Matrix)
   library(rhdf5)
@@ -22,7 +22,6 @@ write_tome_data <- function(exon_mat = NULL,
     stop("Provide at least one of exon_mat or intron_mat.")
   }
 
-  # Modified from cellrangerRkit
   H5close()
   if (file.exists(tome) & overwrite) {
     # Delete old version and make a new file
@@ -65,149 +64,151 @@ write_tome_data <- function(exon_mat = NULL,
   root <- H5Fopen(tome, flags = "H5F_ACC_RDWR")
   H5Fclose(root)
 
+  h5CreateGroup(tome, "data")
+
   ## Exon Data
   if(!is.null(exon_mat)) {
-    h5createGroup(tome, "exon")
-    h5createGroup(tome, "t_exon")
+    h5createGroup(tome, "data/exon")
+    h5createGroup(tome, "data/t_exon")
     suppressWarnings({
       # Rows = Samples, Columns = Genes (Fast gene retrieval)
-      print("Writing exon/x.")
+      print("Writing data/exon/x.")
       # data values
       h5createDataset(tome,
-                      dataset = "exon/x",
+                      dataset = "data/exon/x",
                       dims = length(exon_mat@x),
                       chunk = 1000,
                       level = compression_level)
       h5write(exon_mat@x,
               tome,
-              "exon/x")
+              "data/exon/x")
 
       # data indices
-      print("Writing exon/i.")
+      print("Writing data/exon/i.")
       h5createDataset(tome,
-                      dataset = "exon/i",
+                      dataset = "data/exon/i",
                       dims = length(exon_mat@x),
                       chunk = 1000, level = compression_level)
       h5write(exon_mat@i,
               tome,
-              "exon/i")
+              "data/exon/i")
 
       # data index pointers
-      print("Writing exon/p.")
+      print("Writing data/exon/p.")
       h5write(exon_mat@p,
               tome,
-              "exon/p")
+              "data/exon/p")
 
       h5write(c(nrow(exon_mat), ncol(exon_mat)),
               tome,
-              "exon/dims")
+              "data/exon/dims")
 
       # Rows = Genes, Columns = Samples (Fast sample retrieval)
 
       # t_data values
-      print("Writing t_exon/x.")
+      print("Writing data/t_exon/x.")
       h5createDataset(tome,
-                      dataset = "t_exon/x",
+                      dataset = "data/t_exon/x",
                       dims = length(t_exon_mat@x),
                       chunk = 1000,
                       level = compression_level)
       h5write(t_exon_mat@x,
               tome,
-              "t_exon/x")
+              "data/t_exon/x")
 
       # t_data indices
-      print("Writing t_exon/i.")
+      print("Writing data/t_exon/i.")
       h5createDataset(tome,
-                      dataset = "t_exon/i",
+                      dataset = "data/t_exon/i",
                       dims = length(t_exon_mat@i),
                       chunk = 1000,
                       level = compression_level)
       h5write(t_exon_mat@i,
               tome,
-              "t_exon/i")
+              "data/t_exon/i")
 
       # t_data index pointers
-      print("Writing t_exon/p.")
+      print("Writing data/t_exon/p.")
       h5write(t_exon_mat@p,
               tome,
-              "t_exon/p")
+              "data/t_exon/p")
       h5write(c(nrow(t_exon_mat), ncol(t_exon_mat)),
               tome,
-              "t_exon/dims")
+              "data/t_exon/dims")
     })
   }
 
   if(!is.null(intron_mat)) {
-    h5createGroup(tome, "intron")
-    h5createGroup(tome, "t_intron")
+    h5createGroup(tome, "data/intron")
+    h5createGroup(tome, "data/t_intron")
 
     suppressWarnings({
 
       ## Intron data
       # Rows = Samples, Columns = Genes (Fast gene retrieval)
-      print("Writing intron/x.")
+      print("Writing data/intron/x.")
       # data values
       h5createDataset(tome,
-                      dataset = "intron/x",
+                      dataset = "data/intron/x",
                       dims = length(intron_mat@x),
                       chunk = 1000,
                       level = compression_level)
       h5write(intron_mat@x,
               tome,
-              "intron/x")
+              "data/intron/x")
 
       # data indices
-      print("Writing intron/i.")
+      print("Writing data/intron/i.")
       h5createDataset(tome,
-                      dataset = "intron/i",
+                      dataset = "data/intron/i",
                       dims = length(intron_mat@x),
                       chunk = 1000, level = compression_level)
       h5write(intron_mat@i,
               tome,
-              "intron/i")
+              "data/intron/i")
 
       # data index pointers
-      print("Writing intron/p.")
+      print("Writing data/intron/p.")
       h5write(intron_mat@p,
               tome,
-              "intron/p")
+              "data/intron/p")
 
       h5write(c(nrow(intron_mat), ncol(intron_mat)),
               tome,
-              "intron/dims")
+              "data/intron/dims")
 
       # Rows = Genes, Columns = Samples (Fast sample retrieval)
 
       # t_data values
-      print("Writing t_intron/x.")
+      print("Writing data/t_intron/x.")
       h5createDataset(tome,
-                      dataset = "t_intron/x",
+                      dataset = "data/t_intron/x",
                       dims = length(t_intron_mat@x),
                       chunk = 1000,
                       level = compression_level)
       h5write(t_intron_mat@x,
               tome,
-              "t_intron/x")
+              "data/t_intron/x")
 
       # t_data indices
-      print("Writing t_intron/i.")
+      print("Writing data/t_intron/i.")
       h5createDataset(tome,
-                      dataset = "t_intron/i",
+                      dataset = "data/t_intron/i",
                       dims = length(t_intron_mat@i),
                       chunk = 1000,
                       level = compression_level)
       h5write(t_intron_mat@i,
               tome,
-              "t_intron/i")
+              "data/t_intron/i")
 
       # t_data index pointers
-      print("Writing t_intron/p.")
+      print("Writing data/t_intron/p.")
       h5write(t_intron_mat@p,
               tome,
-              "t_intron/p")
+              "data/t_intron/p")
       h5write(c(nrow(t_intron_mat), ncol(t_intron_mat)),
               tome,
-              "t_intron/dims")
+              "data/t_intron/dims")
     })
   }
 
@@ -239,19 +240,19 @@ write_tome_data <- function(exon_mat = NULL,
   if(!is.null(exon_mat)) {
     print("Calculating total exon counts per sample")
     total_exon_counts <- unname(unlist(apply(t_exon_mat, 2, sum)))
-    print("Writing total_exon_counts.")
+    print("Writing data/total_exon_counts.")
     h5write(total_exon_counts,
             tome,
-            "total_exon_counts")
+            "data/total_exon_counts")
   }
 
   if(!is.null(intron_mat)) {
     print("Calculating total intron counts per sample")
     total_intron_counts <- unname(unlist(apply(t_intron_mat, 2, sum)))
-    print("Writing total_intron_counts.")
+    print("Writing data/total_intron_counts.")
     h5write(total_intron_counts,
             tome,
-            "total_intron_counts")
+            "data/total_intron_counts")
   }
 
   H5close()
