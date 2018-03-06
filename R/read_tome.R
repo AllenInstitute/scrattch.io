7be7847975832328897dbd08bb6c8190e5c63ae3
Lucas Gray <soulcatalyst@gmail.com>
2018-03-06 13:25:58 -0800

    fixing read_tome_exon_lengths

--- R/read_tome.R
+++ R/read_tome.R
@@ -173,7 +173,7 @@ read_tome_exon_lengths <- function(tome,
   }
 
   if(!is.null(genes)) {
-    exon_lengths <- exon_lengths[gene_name %in% genes,]
+    exon_lengths <- exon_lengths[exon_lengths$gene_name %in% genes,]
   }
 
   H5close()
@@ -208,7 +208,7 @@ read_tome_intron_lengths <- function(tome,
   }
 
   if(!is.null(genes)) {
-    intron_lengths <- intron_lengths[gene_name %in% genes,]
+    intron_lengths <- intron_lengths[intron_lengths$gene_name %in% genes,]
   }
 
   H5close()
