a1ddeafd24da317c183208db68ce79c182ae7107
Lucas Gray <soulcatalyst@gmail.com>
2018-03-06 12:31:40 -0800

    missing / in read_tome_data.frame

--- R/read_tome_lowlevel.R
+++ R/read_tome_lowlevel.R
@@ -39,7 +39,7 @@ read_tome_data.frame <- function(tome,
 
     df <- map(selected_columns,
               function(x) {
-                h5read(tome, paste0(df_name, x))
+                h5read(tome, paste0(df_name,"/", x))
               }
     )
     names(df) <- selected_columns
