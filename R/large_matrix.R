2e950ba6c9cdac54f4e076b829a53868e37f620e
Lucas Gray <soulcatalyst@gmail.com>
2019-01-11 10:12:55 -0800

    adding large_dgCMatrix_to_matrix()

--- R/large_matrix.R
+++ R/large_matrix.R
@@ -4,7 +4,7 @@
 #'
 #' This works through the matrix in column-based chunks becaues this is much faster for conversion to column-indexed dgCMatrices.
 #'
-#' @param mat The matrix file to convert
+#' @param mat The matrix object to convert
 #' @param chunk_size The number of columns to read as a chunk. For ~30k genes, a chunk of 5000 (the default) is ~1GB in memory.
 #'
 #' @return A dgCMatrix object in the same orientation as the matrix object.
@@ -51,5 +51,62 @@ large_matrix_to_dgCMatrix <- function(mat,
   colnames(all_sparse) <- col_names
 
   return(all_sparse)
+
+}
+
+#' Convert a large dgCMatrix object to a matrix
+#'
+#' May be useful in cases where calling as.matrix() directly gives a "Cholmod error 'problem too large'" error.
+#'
+#' This works through the matrix in column-based chunks becaues this is much faster for conversion from column-indexed dgCMatrices.
+#'
+#' It should still work on other types of Matrix-package classes, but may be slower.
+#'
+#' @param mat The dgCMatrix object to convert
+#' @param chunk_size The number of columns to read as a chunk. For ~30k genes, a chunk of 5000 (the default) is ~1GB in memory, and fits within the CHOLMOD limits.
+#'
+#' @return A standard matrix object in the same orientation as the dgCMatrix object.
+#'
+large_dgCMatrix_to_matrix <- function (mat,
+                                       chunk_size = 5000) {
+
+  row_names <- rownames(mat)
+  col_names <- colnames(mat)
+  n_row <- nrow(mat)
+  n_col <- ncol(mat)
+
+  print("Allocating initial dense matrix")
+  all_dense <- matrix(0, ncol = n_col, nrow = n_row)
+
+  n_chunks <- floor(n_col/chunk_size)
+  chunk <- 1
+  chunk_start <- 1 + chunk_size * (chunk - 1)
+  chunk_end <- chunk_size * chunk
+  print(paste0("Adding cols ", chunk_start, " to ", chunk_end))
+
+  chunk_mat <- as(mat[, chunk_start:chunk_end], "matrix")
+  all_dense[, chunk_start:chunk_end] <- chunk_mat
+
+  for (chunk in 2:n_chunks) {
+    chunk_start <- 1 + chunk_size * (chunk - 1)
+    chunk_end <- chunk_size * chunk
+    print(paste0("Adding cols ", chunk_start, " to ", chunk_end))
+    chunk_mat <- as(mat[,chunk_start:chunk_end], "matrix")
+    all_dense[,chunk_start:chunk_end] <- chunk_mat
+  }
+  chunk_start <- n_chunks * chunk_size + 1
+  chunk_end <- n_col
+  print(paste0("Adding cols ", chunk_start, " to ", chunk_end))
+  chunk_mat <- as(mat[, (n_chunks * chunk_size + 1):n_col], "matrix")
+  all_dense[,chunk_start:chunk_end] <- chunk_mat
+
+  if(!is.null(row_names)) {
+    rownames(out_mat) <- row_names
+  }
+  if(!is.null(col_names)) {
+    colnames(out_mat) <- col_names
+  }
+
+  return(all_dense)
 
 }
